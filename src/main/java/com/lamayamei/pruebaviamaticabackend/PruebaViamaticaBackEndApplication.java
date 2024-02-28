726887bfe95371d960b1ccbbdf510837ab9aafdd
yameilama <144568933+yameilama@users.noreply.github.com>
2024-02-28 07:55:21 -0600

    Initial commit

---
+++ src/main/java/com/lamayamei/pruebaviamaticabackend/PruebaViamaticaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.lamayamei.pruebaviamaticabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaViamaticaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaViamaticaBackEndApplication.class, args);
+	}
+
+}
